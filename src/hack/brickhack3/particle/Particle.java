96c67b803e0c9c16641a17397ef00cdaa0b4745a
thatging3rkid <thatging3rkid@users.noreply.github.com>
2017-02-11 14:37:07 -0500

    started on the particle class

--- src/hack/brickhack3/particle/Particle.java
+++ src/hack/brickhack3/particle/Particle.java
@@ -4,4 +4,28 @@ package hack.brickhack3.particle;
  * Created by Connor on 2/11/2017.
  */
 public class Particle {
+
+    private double x;
+    private double y;
+    private double velocityCompX;
+    private double velocityCompY;
+
+    public Particle(double x, double y) {
+        this.x = x;
+        this.y = y;
+
+    }
+
+    /**
+     * Sets the velocity of this particle
+     *
+     * @param magnitude a magnitude
+     * @param direction a direction (in degrees)
+     */
+    public void setVelocity(double magnitude, double direction) {
+        this.velocityCompX = magnitude * Math.cos(Math.toRadians(direction));
+        this.velocityCompY = magnitude * Math.sin(Math.toRadians(direction));
+    }
+    
+    
 }
