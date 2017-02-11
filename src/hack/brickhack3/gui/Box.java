b54017daf95d04c622067b6d454e7fd428fc66b5
BuildTools <unconfigured@null.spigotmc.org>
2017-02-11 13:50:41 -0500

    Second Commit

--- src/hack/brickhack3/gui/Box.java
+++ src/hack/brickhack3/gui/Box.java
@@ -1,7 +1,24 @@
 package hack.brickhack3.gui;
 
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+
 /**
  * Created by Connor on 2/11/2017.
  */
 public class Box {
+    public void start(){
+        try{
+            Display.setDisplayMode(new DisplayMode(800,800));
+            Display.create();
+        } catch (LWJGLException e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+        while (Display.isCloseRequested()){
+            Display.update();
+        }
+        Display.destroy();
+    }
 }
---
+++ src/hack/brickhack3/startup/Main.java
@@ -0,0 +1,14 @@
+package hack.brickhack3.startup;
+
+import hack.brickhack3.gui.Box;
+import hack.brickhack3.particle.Particle;
+
+/**
+ * Created by seanb on 2/11/2017.
+ */
+public class Main {
+    public static void main(String[] args){
+        Box box = new Box();
+        Particle particle = new Particle();
+    }
+}
