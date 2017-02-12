ad5a02b42bf6e4c39c25fabd063e2e78f021db29
thatging3rkid <thatging3rkid@users.noreply.github.com>
2017-02-11 20:56:55 -0500

    moved code to box class

--- src/hack/brickhack3/gui/Box.java
+++ src/hack/brickhack3/gui/Box.java
@@ -3,6 +3,8 @@ package hack.brickhack3.gui;
 import hack.brickhack3.particle.Particle;
 import org.lwjgl.opengl.GL11;
 
+import java.util.ArrayList;
+
 /**
  * Created by seanb on 2/11/2017.
  */
@@ -13,6 +15,11 @@ public class Box {
     private double DEG2RAD = Math.PI / 180;
     public Box(Particle[] particles){
         this.particles = particles;
+    }
+
+    public Box(int num_particles) {
+        this.particles = this.createParticles(num_particles);
+        this.distributeParticles(this.particles);
     }
 
     public static int getWidth() {
@@ -46,4 +53,88 @@ public class Box {
             i += 1;
         }
     }
+
+    /**
+     *  Constants needed for speed and such
+     */
+    private double T = 2093, M = 1, R = 8.3145;
+
+
+    /**
+     * This function describes the distributions of the speed
+     * @param v the given speed
+     * @return the fraction of particles that will have that speed.
+     */
+    private double func(double v){
+        return 4 * Math.PI * Math.pow((M/2 * Math.PI * R * T) , (3/2)) * Math.pow( v , 2 )
+                * Math.exp((-M * Math.pow( v , 2 ) / (2 * R * T)));
+    }
+
+
+    /**
+     * Create an array of particles with weighted speeds and random directions and positions
+     * @param N the number of particles to generate
+     * @return the array full of particles
+     */
+    private Particle[] createParticles(int N){
+        Particle[] arr = new Particle[N];
+        int i = 0;
+
+        double v = 0;
+        while (i < N){
+
+            int j = (int)(func(v) * N);
+            if(j == 0 && i > 0) j = N;  //Catches right end of graph
+            while(j > 0 && i < N){
+
+                //create a particle with speed v
+                arr[i] = new Particle(0, 0, 4);
+                double dir = Math.random() * 2 * Math.PI;
+                arr[i].setVelocity(v, dir);
+
+                j -= 1;
+                i += 1;
+            }
+
+            v += 1;
+        }
+
+        return arr;
+    }
+
+    private void distributeParticles(Particle[] arr) {
+        int N = arr.length;
+        ArrayList<Particle> placed = new ArrayList<Particle>();
+
+        int i = 0;
+        while (i < N) {
+
+            Particle p = arr[i];
+            boolean isSamePos = true;
+            //randomize position
+            while (isSamePos) {
+                isSamePos = false;
+                double x = ((Math.random() * (Box.getWidth() - (2 * p.getRadius()))) + p.getRadius());
+                double y = ((Math.random() * (Box.getHeight() - (2 * p.getRadius()))) + p.getRadius());
+                if (placed.size()==0) {
+                    p.setX(x);
+                    p.setY(y);
+                    placed.add(p);
+                    i++;
+                }
+                else {
+                    for (Particle P : placed) {
+                        if (P.getX() == x && P.getY() == y) isSamePos = true;
+                    }
+                    if (!isSamePos) {
+                        p.setX(x);
+                        p.setY(y);
+                        placed.add(p);
+                        i++;
+                    }
+                }
+            }
+        }
+    }
+
 }
