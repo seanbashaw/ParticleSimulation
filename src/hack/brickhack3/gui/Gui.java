6a4aec62bb994fc83149bff026e791532ed469e1
BuildTools <unconfigured@null.spigotmc.org>
2017-02-12 06:49:03 -0500

    Added FULLSCREEN

--- src/hack/brickhack3/gui/Gui.java
+++ src/hack/brickhack3/gui/Gui.java
@@ -1,6 +1,7 @@
 package hack.brickhack3.gui;
 
 import org.lwjgl.LWJGLException;
+import org.lwjgl.input.Keyboard;
 import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
@@ -10,7 +11,7 @@ public class Gui {
     public Gui(Box p){
         this.box = p;
     }
-
+    private boolean vsync = false;
     private Box box;
     public static int getWidth() {
         return Box.getWidth();
@@ -523,12 +524,11 @@ public class Gui {
         GL11.glEnd();
     }
     public void start() {
-        int sliderLength = 300;
-        int interfaceWidth = 400;
+        int sliderLength = 200;
+        int interfaceWidth = 300;
         int elementLength = 5;
         try {
             Display.setDisplayMode(new DisplayMode(Box.getWidth() + interfaceWidth, Box.getHeight()));
-            Display.setVSyncEnabled(true);
             Display.setFullscreen(true);
             Display.setTitle("Particle Simulator - BrickHacks 3");
             Display.create();
@@ -618,11 +618,77 @@ public class Gui {
                 selectedSlider = -1;
                 toggle=false;
             }
+            while (Keyboard.next()) {
+                if (Keyboard.getEventKeyState()) {
+                    if (Keyboard.getEventKey() == Keyboard.KEY_F) {
+                        setDisplayMode(800, 600, !Display.isFullscreen());
+                    }
+                    else if (Keyboard.getEventKey() == Keyboard.KEY_V) {
+                        vsync = !vsync;
+                        Display.setVSyncEnabled(vsync);
+                    }
+                }
+            }
             update();
             Display.update();
         }
         Display.destroy();
     }
+
+    public void setDisplayMode(int width, int height, boolean fullscreen) {
+
+        // return if requested DisplayMode is already set
+        if ((Display.getDisplayMode().getWidth() == width) &&
+                (Display.getDisplayMode().getHeight() == height) &&
+                (Display.isFullscreen() == fullscreen)) {
+            return;
+        }
+
+        try {
+            DisplayMode targetDisplayMode = null;
+
+            if (fullscreen) {
+                DisplayMode[] modes = Display.getAvailableDisplayModes();
+                int freq = 0;
+
+                for (int i=0;i<modes.length;i++) {
+                    DisplayMode current = modes[i];
+
+                    if ((current.getWidth() == width) && (current.getHeight() == height)) {
+                        if ((targetDisplayMode == null) || (current.getFrequency() >= freq)) {
+                            if ((targetDisplayMode == null) || (current.getBitsPerPixel() > targetDisplayMode.getBitsPerPixel())) {
+                                targetDisplayMode = current;
+                                freq = targetDisplayMode.getFrequency();
+                            }
+                        }
+
+                        // if we've found a match for bpp and frequence against the
+                        // original display mode then it's probably best to go for this one
+                        // since it's most likely compatible with the monitor
+                        if ((current.getBitsPerPixel() == Display.getDesktopDisplayMode().getBitsPerPixel()) &&
+                                (current.getFrequency() == Display.getDesktopDisplayMode().getFrequency())) {
+                            targetDisplayMode = current;
+                            break;
+                        }
+                    }
+                }
+            } else {
+                targetDisplayMode = new DisplayMode(width,height);
+            }
+
+            if (targetDisplayMode == null) {
+                System.out.println("Failed to find value mode: "+width+"x"+height+" fs="+fullscreen);
+                return;
+            }
+
+            Display.setDisplayMode(targetDisplayMode);
+            Display.setFullscreen(fullscreen);
+
+        } catch (LWJGLException e) {
+            System.out.println("Unable to setup mode "+width+"x"+height+" fullscreen="+fullscreen + e);
+        }
+    }
+
     private void update() {
         box.update();
     }
